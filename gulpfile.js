fa34925f0889d807a0058bc41a8de126dce56ce6
Hunter <20121611@leeds.gov.uk>
2016-08-18 16:27:46 +0100

    Added HTML, CSS and JavaScript files from the original prototype

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var gutil = require('gulp-util');
+var sassInheritance = require('gulp-sass-inheritance');
+var gulpif = require('gulp-if');
+var connect = require('gulp-connect');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+var jasmine = require('gulp-jasmine');
+
+var paths = {
+  source: {scripts: './javascript/**/*.js', stylesheets: './stylesheets/**/*.scss', html: './*.html'},
+  target: {scripts: './js', stylesheets: './css'}
+};
+
+gulp.task('webserver', function() {
+    connect.server({livereload: true});
+});
+
+gulp.task('lint', function() {
+  return gulp.src(paths.source.scripts)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+});
+
+gulp.task('minifyjs', ['lint'], function () {
+  return gulp.src(paths.source.scripts)
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(rename({
+            suffix: '.min'
+        }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.target.scripts));
+});
+
+gulp.task('sass', function () {
+    return gulp.src(paths.source.stylesheets)
+      .pipe(sourcemaps.init())    
+      .pipe(sass().on('error', function (err) {
+          notify({ title: 'SASS Task' }).write(err.line + ': ' + err.message);
+          this.emit('end');
+      }))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('./css'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.source.html, ['html:watch']);
+    gulp.watch(paths.source.stylesheets, ['sass:watch']);
+    gulp.watch(paths.source.scripts, ['minifyjs']);
+});
+
+var inheritanceCondition = function (file) {
+    //as a result of this, only partials will trigger inheritance compiling
+    //i.e. if a full file (not a partial) (1) is imported by another (2), changes to (1) will not trigger compilation in (2)
+    var filepath = file.history[0];
+    var filename = filepath.replace(/^.*[\\\/]/, '');
+    return /\/_/.test(filename) || /^_/.test(filename); // check whether partial (starts with '_');
+};
+
+gulp.task('html:watch', function() {
+    return gulp.src(paths.source.html)
+        .pipe(connect.reload());
+});
+
+gulp.task('sass:watch', function() {
+    return gulp.src(paths.source.stylesheets)
+        .pipe(sourcemaps.init())    
+        .pipe(plumber())
+        .pipe(gulpif(inheritanceCondition, sassInheritance({dir: 'stylesheets/'})))
+        .pipe(sass().on('error', function (err) {
+            notify({ title: 'SASS Task' }).write(err.line + ': ' + err.message);
+            this.emit('end');
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.target.stylesheets))
+        .pipe(connect.reload());
+});
+
+gulp.task('test', function () {
+  return gulp.src('./spec/*.js')
+        .pipe(jasmine())
+});
+
+gulp.task('default', ['lint', 'test', 'webserver', 'watch']);
